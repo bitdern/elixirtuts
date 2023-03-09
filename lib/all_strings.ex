607f07ff86ccf260d93a782f6a289e6613cf703d
bitdern <acnewell09@gmail.com>
2023-03-09 17:12:47 -0600

    finished the functions, documentation and space craft challenge tomorrow

--- lib/all_strings.ex
+++ lib/all_strings.ex
@@ -4,4 +4,19 @@ defmodule AllStrings do
     String.trim(value)
     |> String.first()
   end
+
+  def initial(value) do
+    "#{first_letter(value) |> String.capitalize()}. "
+  end
+
+  def initials(full_name) do
+    list = String.split(full_name)
+
+    Enum.map(list, fn name ->
+      initial(name)
+    end)
+    |> List.to_string()
+    |> String.trim()
+
+  end
 end
