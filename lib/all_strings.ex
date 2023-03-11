aa5efe18aab4e2cbb3d6b93e8c04892e1fa34dca
bitdern <acnewell09@gmail.com>
2023-03-11 09:21:01 -0600

    finished docs & tests

--- lib/all_strings.ex
+++ lib/all_strings.ex
@@ -55,4 +55,64 @@ defmodule AllStrings do
     |> String.trim()
 
   end
+
+ @doc """
+  Returns a flying saucer string.
+  Returns `string`
+  ## Examples
+      iex> AllStrings.build_saucer()
+      ~s(\\n                    %%%%%%%%%%%%%%%%%%%%\\n               %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\\n            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\\n      ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\\n  ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\\n************************************************************\\n    ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\\n       ,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,\\n            ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\\n\\n)
+  """
+  def build_saucer() do
+    """
+                        %%%%%%%%%%%%%%%%%%%%
+                   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+          ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+      ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+    ************************************************************
+        ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+           ,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,
+                ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+    """
+  end
+
+  @doc """
+  Returns the `intials`'s SPACECRAFT of given `full_name` capitalized with a . and space after each letter except last.
+  Returns `string`
+  ## Parameters
+  - full_name: string
+  ## Examples
+      iex> AllStrings.customized_spacecraft("blork erlang")
+      ~s(\\n                    %%%%%%%%%%%%%%%%%%%%\\n               %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\\n            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\\n      ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\\n  ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\\n******************** B. E.'s  SPACECRAFT *******************\\n    ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\\n       ,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,,,,&&&,,,\\n            ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\\n\\n)
+  """
+  def customized_spacecraft(full_name) do
+    spacecraft_label = "******************** #{initials(full_name)}'s  SPACECRAFT *******************"
+    "#{build_saucer()}"
+    |> String.replace("************************************************************", spacecraft_label)
+  end
+
+  @doc """
+  Returns a flying saucer output.
+  Returns `atom`
+  ## Examples
+      iex> AllStrings.display_saucer()
+      :ok
+  """
+  def display_saucer() do
+    IO.puts(build_saucer())
+  end
+
+  @doc """
+  Returns a `intials`'s SPACECRAFT of given `full_name` capitalized with a . output.
+  Returns `atom`
+  ## Parameters
+  - full_name: string
+  ## Examples
+      iex> AllStrings.display_custom_spacecraft("blork erlang")
+      :ok
+  """
+  def display_custom_spacecraft(full_name) do
+    IO.puts(customized_spacecraft(full_name))
+  end
 end
