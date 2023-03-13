eea79eaeae7f68c336687bc1d0a73a5e8064cc8c
bitdern <acnewell09@gmail.com>
2023-03-13 15:30:38 -0500

    added some documentation

---
+++ lib/equipment_details.ex
@@ -0,0 +1,46 @@
+defmodule EquipmentDetails do
+
+@doc """
+  Returns the `equipment list`.
+  Returns `list` of `atoms`
+  ## Examples
+      iex> EquipmentDetails.get_equipment_list
+        [:space_helmet, :space_suit, :snacks, :grappling_hook, :probe]
+  """
+  def get_equipment_list() do
+    [:space_helmet, :space_suit, :snacks, :grappling_hook, :probe]
+  end
+
+  @doc """
+  Returns information about a single item in the `list`.
+  Returns `tuple` of `atoms`
+  ## Parameters
+  - list: list
+  ## Examples
+      iex> EquipmentDetails.item_detail(:snacks)
+    {1, :kg, 16}
+  """
+  def item_details(:space_helmet) do
+    {3, :kg, 1}
+  end
+
+  def item_details(:space_suit) do
+    {16, :kg, 1}
+  end
+
+  def item_details(:snacks) do
+    {1, :kg, 16}
+  end
+
+  def item_details(:grappling_hook) do
+    {4, :kg, 1}
+  end
+
+  def item_details(:probe) do
+    {2, :kg, 1}
+  end
+
+  def item_details(_other) do
+    raise "unknown item"
+  end
+end
